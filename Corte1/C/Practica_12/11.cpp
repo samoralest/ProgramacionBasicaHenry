4f19d83aa788c89edd7d7e883674aeb8ba0d572f
samoralest <moralestorosantiago@gmail.com>
2025-10-07 20:29:08 -0500

    Add files via upload

---
+++ Corte1/C/Practica_12/11.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char contrasenaCorrecta[] = "santi17"; 
+    char intento[50];
+    int i, exito = 0;
+
+    printf("Tienes 3 intentos para ingresar la contraseña.\n");
+
+    for (i = 1; i <= 3; i++) {
+        printf("Intento %d: ", i);
+        scanf("%s", intento);
+
+        if (strcmp(intento, contrasenaCorrecta) == 0) {
+            printf("Contraseña Correcta\n");
+            exito = 1;  
+            break;     
+        } else {
+            printf("Lo siento, contraseña equivocada\n");
+        }
+    }
+
+    if (!exito) {
+        printf("Se agotaron los intentos.\n");
+    }
+
+    return 0;
+}
